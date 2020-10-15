9988cfdd959bf9746550743dbd7c93578b271e09
AliHus <alihus@dhcp-wifi-8021x-155-41-48-249.bu.edu>
2020-10-15 12:33:25 -0400

    Added modified files oct15

--- 6oct/simpleNodeProject/routes/passingArgs.js
+++ 6oct/simpleNodeProject/routes/passingArgs.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 router.route('/')
 .get((req, res, next) => {
-    console.log(`${req.url} ${req.method}`)
+    console.log(`Got ${req.query.name} and ${req.query.dept} on ${req.url} ${req.method}`)
+    console.log(`Here's the list of params:\n`)
+    for (key in req.query){
+        console.log(`${key}`);
+    }
+})
+
+.post((req, res, next) => {
+    console.log(`Got ${req.query.name} and ${req.query.dept} on ${req.url} ${req.method}`)
+    req.params.name=req.body.name;
+    req.isAuthenticated = false;
+    console.log('');
+    res.render('passingArgs', {'name': req.body.name})
 
 })
-.post()
+module.exports = router;
