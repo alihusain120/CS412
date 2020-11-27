223a32e583863716e9c9026fa7a811e7c4551086
alihus <alihusain120@gmail.com>
2020-11-27 16:37:14 -0500

    Added redis stuff but PS4 part is still broken

--- routes/ps4.js
+++ routes/ps4.js
@@ -3,12 +3,45 @@ var router = express.Router();
 const APICONFIG = require('../config/config');
 const fetch = require("node-fetch");
 
+const redis = require('redis');
+const client = redis.createClient();
+const {promisify} = require('util');
+
+const asyncSet = promisify(client.set).bind(client);
+const asyncGet = promisify(client.get).bind(client);
+const asyncExists = promisify(client.exists).bind(client);
+const asyncExpire = promisify(client.expire).bind(client);
+
+client.flushdb((err, response) => {
+    if (err) {throw new Error('Flushing error')}
+})
+
 router.post('/', async (req, res) => {
     const apivalue = await fetch(APICONFIG.url + APICONFIG.key + '&q=' + req.body.city)
-    request(apivalue, function (error, response) {
-        if (error) throw new Error(error);
-        console.log(response.body);
-    });
+
+    if (await asyncExists(apivalue)){
+
+        let data = asyncGet(apivalue);
+        let response = {
+            value: data,
+            cached: true
+        }
+        res.json(response);
+
+    } else {
+
+        let data = request(apivalue, function (error, response) {
+            if (error) throw new Error(error);
+            console.log(response.body);
+        });
+        await asyncSet(apivalue, data);
+        let response = {
+            value: data,
+            cached: false
+        }
+        res.json(response);
+    }
+
 })
 
 async function postData(url = '', data = {}) {
