292fbacd4cb60d9f944f98ddfce43311e3e87eee
alihus <alihusain120@gmail.com>
2020-12-15 19:30:02 -0500

    Can't get it going, tapping out. Thanks for the semester.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {rates} from './data/EXRateMOCK';
 import {EXRate} from './data/EXRate';
+import {Country} from './data/Country';
+import {AlphavantageService} from './alphavantage.service';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +13,38 @@ export class AppComponent implements OnInit{
   rates: EXRate[] = rates;
   dailyExRate: EXRate;
   selectedDayRate: string;
+  country: string;
+  countryExRate: string;
+  timeOfRate: string;
 
-  constructor() { }
+  constructor(private AVService: AlphavantageService) { }
 
   ngOnInit(): void {
   }
 
   getExRate(ex: EXRate): void {
     this.selectedDayRate = ex.EURUSD;
+  }
+
+  //Tried best, can't get it going.
+  //No linter, I will not put space at beginning of comment.
+
+  getCountry(): void {
+    if (this.country === 'Japan'){
+      this.AVService.getUSDtoYen().subscribe(
+        response => {
+          this.countryExRate = response['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+          this.timeOfRate = response['Realtime Currency Exchange Rate']['6. Last Refreshed'];
+        }
+      );
+    } else {
+      this.AVService.getUSDtoGBP().subscribe(
+        response => {
+          this.countryExRate = response['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+          this.timeOfRate = response['Realtime Currency Exchange Rate']['6. Last Refreshed'];
+        }
+      );
+    }
   }
 }
 
